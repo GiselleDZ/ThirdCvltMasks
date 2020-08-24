77b49199cae4b0c5765807df110ba8277ceea6f2
GiselleDZ <gz@gisellezatonyl.com>
2020-08-24 15:31:19 -0400

    secured

--- server.js
+++ server.js
@@ -1,13 +1,29 @@
-const express = require('express');
-const path = require('path');
-const port = process.env.PORT || 8080;
-const app = express();
+const express = require('express')
+const path = require('path')
+const port = process.env.PORT || 8080
+const app = express()
+const helmet = require('helmet')
+const session = require('express-session')
+
 
 // the __dirname is the current directory from where the script is running
-app.use(express.static(__dirname));
+app.use(express.static(__dirname))
 
 // send the user to index html page inspite of the url
-app.use(express.static(__dirname + 'index.html'));
+app.use(express.static(__dirname + 'index.html'))
 
+app.use(helmet())
 
-app.listen(port);
+app.set('trust proxy', 1)
+
+app.use(session({
+    secret: process.env.COOKIE,
+    cookie: { 
+        SameSite: "none",
+        secure: true,
+        httpOnly: true,
+     }
+}))
+
+    
+app.listen(port)
